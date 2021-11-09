8d2fc75ff22dee248c5b84f3a0bd2cc31a1d70ce
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2021-11-10 00:44:20 +0600

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("What's your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+Console.ReadKey(true);
